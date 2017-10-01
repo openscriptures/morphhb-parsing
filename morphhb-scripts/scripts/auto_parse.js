ecc39416eb768f5d88134542795a5c17ccf0870f
Huberts <huberts@Hubertss-MacBook-Pro.local>
2017-10-01 16:45:53 +0300

    auto parse in lemma-specific way

--- morphhb-scripts/scripts/auto_parse.js
+++ morphhb-scripts/scripts/auto_parse.js
@@ -1,6 +1,6 @@
 // TODO
-  // separate between Hebrew and Aramaic
-  // 2chron messed up lemma data!
+  // separate between Hebrew and Aramaic so that the same form with different lemma does not disqualify auto-parsing
+  // 2chron seems to have a lot of messed up lemma data
 
 const utils = require('../utils')
 
@@ -26,44 +26,40 @@ module.exports = (connection, done) => {
 
         const rowWithoutMorph = result.shift()
 
-        let checkSingleLemmaStatement = `SELECT DISTINCT lemma FROM words_enhanced WHERE accentlessword="${rowWithoutMorph.accentlessword}"`
-        connection.query(checkSingleLemmaStatement, (err, result) => {
+        // Only uses forms which have been parsed 2+ times by humans, are not flagged as questionable, and in every
+        // instance they have been parsed the same
+        let selectWord = `SELECT morph, COUNT(morph) as cnt FROM words_enhanced
+                            WHERE
+                              accentlessword="${rowWithoutMorph.accentlessword}"
+                              AND lemma="${rowWithoutMorph.lemma}"
+                              AND morph IS NOT NULL
+                              AND status IN ('single', 'confirmed', 'verified')
+                            GROUP BY morph`
+        connection.query(selectWord, (err, result) => {
           if(err) throw err
-
-          if(result.length > 1) {
-            // unsafe to try to auto-parse a word with multiple lemmas
-            console.log(`  Skipping ${rowWithoutMorph.accentlessword} since this accentless form is a representation of multiple lemmas and thus unsafe to auto-parse.`)
-            tryNextWord()
-            return 
+  
+          if(result.length == 1 && result[0].cnt >= 2) {
+            uniqueWordsCount++
+            updateWordQueries.push(`UPDATE words_enhanced SET morph='${result[0].morph}'
+                                    WHERE
+                                      accentlessword="${rowWithoutMorph.accentlessword}"
+                                      AND lemma="${rowWithoutMorph.lemma}"
+                                      AND morph IS NULL
+                                  `)
           }
-          
-          // Only uses forms which have been parsed 2+ times by humans, are not flagged as questionable, and in every
-          // instance they have been parsed the same
-          let selectWord = `SELECT morph, COUNT(morph) as cnt FROM words_enhanced
-                              WHERE accentlessword="${rowWithoutMorph.accentlessword}" AND morph IS NOT NULL AND status IN ('single', 'confirmed', 'verified')
-                              GROUP BY morph`
-          connection.query(selectWord, (err, result) => {
-            if(err) throw err
-    
-            if(result.length == 1 && result[0].cnt >= 2) {
-              uniqueWordsCount++
-              updateWordQueries.push(`UPDATE words_enhanced SET morph='${result[0].morph}'
-                                      WHERE accentlessword="${rowWithoutMorph.accentlessword}" AND morph IS NULL`)
-            }
 
 
-            // Flag for manual check if there is only one outlier for a form that appears a lot, as I would hate for this
-            // to ruin the auto-parsing
-            if(result.length == 2 && (
-              (result[0].cnt == 1 && result[1].cnt > 10)
-              || (result[1].cnt == 1 && result[0].cnt > 10)
-            )) {
-              console.log(`  >> MANUAL CHECK: See if the outlier for the accentless form ${rowWithoutMorph.accentlessword} is invalid, and if so flag it as questionable so that this form can be auto-parsed.`)
-            }
+          // Flag for manual check if there is only one outlier for a form that appears a lot, as I would hate for this
+          // to ruin the auto-parsing
+          if(result.length == 2 && (
+            (result[0].cnt == 1 && result[1].cnt > 10)
+            || (result[1].cnt == 1 && result[0].cnt > 10)
+          )) {
+            console.log(`  >> MANUAL CHECK: See if the outlier for the accentless form ${rowWithoutMorph.accentlessword} is invalid, and if so flag it as questionable so that this form can be auto-parsed.`)
+          }
 
-            tryNextWord()
-    
-          })
+          tryNextWord()
+  
         })
 
       } else {  // done composing updates
@@ -82,8 +78,6 @@ module.exports = (connection, done) => {
 
     tryNextWord()
 
-  })      
-
-
+  })
 
 }
