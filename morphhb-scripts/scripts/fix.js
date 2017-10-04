d89d38e233496bcb4f44c24ec5b1ac80c06be9fc
Huberts <huberts@Hubertss-MacBook-Pro.local>
2017-10-04 10:05:10 +0300

    Added fix scripts

--- morphhb-scripts/scripts/fix.js
+++ morphhb-scripts/scripts/fix.js
@@ -8,7 +8,7 @@ module.exports = (connection, done) => {
 
     (x, next) => {
       
-      console.log(`  Words that end with a state of determined will be changed to absolute...`)
+      console.log(`  Nouns, adjectives and participles that end with a state of determined will be changed to absolute...`)
 
       // N???d
       // A???d
@@ -106,23 +106,311 @@ module.exports = (connection, done) => {
 
     },
 
-    // Nouns, adjectives and participles will be marked absolute if their state is not indicated...
-    // Verbs marked perfect 3rd person plural masculine will be corrected to common gender
-    // Verbs marked imperfect 3rd person plural common will be corrected to masculine gender
-    // Verbs marked sequential (perfect or imperfect) which are missing the indication of the conjunction will be corrected to include the conjunction. Eg. HVqw3fs -> HC/Vqw3fs
-    // Multi-part words with the last part marked a personal pronoun, and with an adjective, noun, preposition or verb in the second to last part, will be corrected to be a pronominal suffix
-    // Nouns, adjectives and participles with a pronominal suffix and marked with a state of absolute will be corrected to have a state of construct
-    // Words with definite article and marked construct will be corrected to be marked absolute
-    // Nouns with a common gender will be corrected to both
-    // Demonstrative, relative and personal pronouns, pronominal suffixes and verbs with a both gender will be corrected to common
-    // Demonstrative and relative pronouns will have person removed (marked ‘x’) if it is indicated
-    // Indefinite and interrogative pronouns will have unneeded parsing info removed and be reduced to Pf and Pi
-    // Directional/paragogic ה and ן will have unneeded parsing info removed and be reduced to Sd, Sh or Sn
-    // Perfect and imperfect verbs (including sequentials), pronouns and pronominal suffixes in 1st person will be marked common gender
-    // Proper nouns (Np) will have unneeded parsing info (anything after the p) removed
-    // Infinitive constructs (V?c) and absolutes (V?a) will have unneeded parsing info (anything after the c or a) removed
-    // Adjectives, pronouns, pronominal suffixes and verbs marked with a number of dual will be corrected to plural
-    // Every double / will be changed to a single /. Eg. HR//Ncfpc -> HR/Ncfpc
+    (x, next) => {
+
+      console.log(`  Nouns, adjectives and participles will be marked construct if their state is not indicated and they have a pronominal suffix...`)
+    
+      // N???/Sp
+      // A???/Sp
+      // V?[rs]??/Sp
+
+      utils.runReplaceOnMorph({
+        connection,
+        table: 'notes',
+        regex: /^(H(?:[^\/]*\/)*(?:N[^\/][^\/][^\/]|A[^\/][^\/][^\/]|V[^\/][rs][^\/][^\/]))(\/Sp)/,
+        replace: '$1c$2',
+        doVerified: true,
+        next,
+      })
+
+    },
+
+    (x, next) => {
+
+      console.log(`  Nouns, adjectives and participles will be marked absolute if their state is not indicated and they do not have a pronominal suffix...`)
+    
+      // N???
+      // A???
+      // V?[rs]??
+
+      utils.runReplaceOnMorph({
+        connection,
+        table: 'notes',
+        regex: /^(H(?:[^\/]*\/)*(?:N[^\/][^\/][^\/]|A[^\/][^\/][^\/]|V[^\/][rs][^\/][^\/]))(\/|$)/,
+        replace: '$1a$2',
+        doVerified: true,
+        next,
+      })
+
+    },
+
+    (x, next) => {
+
+      console.log(`  Verbs marked perfect (including sequentials) 3rd person plural masculine will be corrected to common gender...`)
+    
+      // V?[pq]3mp
+
+      utils.runReplaceOnMorph({
+        connection,
+        table: 'notes',
+        regex: /^(H(?:[^\/]*\/)*V[^\/][pq]3)m(p)/,
+        replace: '$1c$2',
+        doVerified: true,
+        next,
+      })
+
+    },
+
+    (x, next) => {
+
+      console.log(`  Verbs marked imperfect (including sequentials) 3rd person plural common will be corrected to masculine gender...`)
+    
+      // V?[iw]3cp
+
+      utils.runReplaceOnMorph({
+        connection,
+        table: 'notes',
+        regex: /^(H(?:[^\/]*\/)*V[^\/][iw]3)c(p)/,
+        replace: '$1m$2',
+        doVerified: true,
+        next,
+      })
+
+    },
+
+    (x, next) => {
+
+      console.log(`  Verbs marked sequential (perfect or imperfect) which are missing the indication of the conjunction will be corrected to include the conjunction. Eg. HVqw3fs -> HC/Vqw3fs...`)
+    
+      // V?[qw]
+
+      utils.runReplaceOnMorph({
+        connection,
+        table: 'notes',
+        regex: /^(H)(V[^\/][qw])/,
+        replace: '$1C/$2',
+        doVerified: true,
+        next,
+      })
+
+    },
+
+    (x, next) => {
+
+      console.log(`  Multi-part words with the last part marked a personal pronoun, and with an adjective, noun, preposition or verb in the second to last part, will be corrected to be a pronominal suffix...`)
+    
+      // /[ANRV]/Pp???
+
+      utils.runReplaceOnMorph({
+        connection,
+        table: 'notes',
+        regex: /^(H(?:[^\/]*\/)*[ANRV][^\/]*\/)Pp([^\/][^\/][^\/])/,
+        replace: '$1Sp$2',
+        doVerified: true,
+        next,
+      })
+
+    },
+
+    (x, next) => {
+
+      console.log(`  Words with definite article and marked construct will be corrected to be marked absolute...`)
+    
+      // /[ANRV]/Pp???
+
+      utils.runReplaceOnMorph({
+        connection,
+        table: 'notes',
+        regex: /^(H(?:[^\/]*\/)*(?:Rd|Td)\/(?:N[^\/][^\/][^\/]|A[^\/][^\/][^\/]|V[^\/][rs][^\/][^\/]))c/,
+        replace: '$1a',
+        doVerified: true,
+        next,
+      })
+
+    },
+
+    (x, next) => {
+
+      console.log(`  Nouns with a common gender will be corrected to both...`)
+    
+      // N?c
+
+      utils.runReplaceOnMorph({
+        connection,
+        table: 'notes',
+        regex: /^(H(?:[^\/]*\/)*N[^\/])c/,
+        replace: '$1b',
+        doVerified: true,
+        next,
+      })
+
+    },
+
+    (x, next) => {
+
+      console.log(`  Demonstrative, relative and personal pronouns, pronominal suffixes and verbs with a both gender will be corrected to common...`)
+    
+      // P[dpr]?b
+      // Sp?b
+      // V?[rs]b
+      // V?[pqiwhjv]?b
+
+      utils.runReplaceOnMorph({
+        connection,
+        table: 'notes',
+        regex: /^(H(?:[^\/]*\/)*(?:P[dpr][^\/]|Sp[^\/]|V[^\/][rs]|V[^\/][pqiwhjv][^\/]))b/,
+        replace: '$1c',
+        doVerified: true,
+        next,
+      })
+
+    },
+
+    (x, next) => {
+
+      console.log(`  Demonstrative and relative pronouns will have person removed (marked ‘x’) if it is indicated...`)
+    
+      // P[dr][123]
+
+      utils.runReplaceOnMorph({
+        connection,
+        table: 'notes',
+        regex: /^(H(?:[^\/]*\/)*P[dr])[123]/,
+        replace: '$1x',
+        doVerified: true,
+        next,
+      })
+
+    },
+
+    (x, next) => {
+
+      console.log(`  Indefinite and interrogative pronouns will have unneeded parsing info removed and be reduced to Pf and Pi...`)
+    
+      // P[fi]???
+
+      utils.runReplaceOnMorph({
+        connection,
+        table: 'notes',
+        regex: /^(H(?:[^\/]*\/)*P[fi])[^\/]*/,
+        replace: '$1',
+        doVerified: true,
+        next,
+      })
+
+    },
+
+    (x, next) => {
+
+      console.log(`  Directional/paragogic ה and ן will have unneeded parsing info removed and be reduced to Sd, Sh or Sn...`)
+    
+      // P[fi]???
+
+      utils.runReplaceOnMorph({
+        connection,
+        table: 'notes',
+        regex: /^(H(?:[^\/]*\/)*S[dhn])[^\/]*/,
+        replace: '$1',
+        doVerified: true,
+        next,
+      })
+
+    },
+
+    (x, next) => {
+
+      console.log(`  Perfect and imperfect verbs (including sequentials), pronouns and pronominal suffixes in 1st person will be marked common gender...`)
+    
+      // V?[pqiw]1[bmfx]
+      // P?1[bmfx]
+      // Sp1[bmfx]
+
+      utils.runReplaceOnMorph({
+        connection,
+        table: 'notes',
+        regex: /^(H(?:[^\/]*\/)*(?:V[^\/][pqiw]|P[^\/]|Sp)1)[bmfx]/,
+        replace: '$1c',
+        doVerified: true,
+        next,
+      })
+
+    },
+    
+    (x, next) => {
+
+      console.log(`  Proper nouns (Np) will have unneeded parsing info (anything after the p) removed...`)
+    
+      // Np
+
+      utils.runReplaceOnMorph({
+        connection,
+        table: 'notes',
+        regex: /^(H(?:[^\/]*\/)*Np)[^\/]*/,
+        replace: '$1',
+        doVerified: true,
+        next,
+      })
+
+    },
+
+    (x, next) => {
+
+      console.log(`  Infinitive constructs (V?c) and absolutes (V?a) will have unneeded parsing info (anything after the c or a) removed...`)
+    
+      // V?[ca]
+
+      utils.runReplaceOnMorph({
+        connection,
+        table: 'notes',
+        regex: /^(H(?:[^\/]*\/)*V[^\/][ca])[^\/]*/,
+        replace: '$1',
+        doVerified: true,
+        next,
+      })
+
+    },
+
+    (x, next) => {
+
+      console.log(`  Adjectives, pronouns, pronominal suffixes and verbs marked with a number of dual will be corrected to plural...`)
+    
+      // A??d
+      // P???d
+      // Sp??d
+      // V?[rs]?d
+      // V?[pqiwhjv]??d
+
+      utils.runReplaceOnMorph({
+        connection,
+        table: 'notes',
+        regex: /^(H(?:[^\/]*\/)*(?:A[^\/][^\/]|P[^\/][^\/][^\/]|Sp[^\/][^\/]|V[^\/][rs][^\/]|V[^\/][pqiwhjv][^\/][^\/]))d/,
+        replace: '$1p',
+        doVerified: true,
+        next,
+      })
+
+    },
+
+    (x, next) => {
+
+      console.log(`  Every double / will be changed to a single /. Eg. HR//Ncfpc -> HR/Ncfpc...`)
+    
+      // A??d
+      // P???d
+      // Sp??d
+      // V?[rs]?d
+      // V?[pqiwhjv]??d
+
+      utils.runReplaceOnMorph({
+        connection,
+        table: 'notes',
+        regex: /\/\/+/g,
+        replace: '/',
+        doVerified: true,
+        next,
+      })
+
+    },
     
     () => {
       console.log(`Done with fix script.`)
