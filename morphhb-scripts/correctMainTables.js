a404d60887bdadb5a1718892d7063f25e6b25d0e
Huberts <huberts@Hubertss-MacBook-Pro.local>
2017-10-15 02:48:55 +0300

    Beginnings of fix to words and wordnote

--- morphhb-scripts/correctMainTables.js
+++ morphhb-scripts/correctMainTables.js
@@ -17,159 +17,106 @@ connection.connect(function(err) {
 
   console.log(`\nSTARTING`)
 
-  
-  const readyToRunCheck = `
-    SELECT table_name FROM information_schema.tables
-      WHERE table_schema='${process.env.HOSTNAME || 'oshb'}' AND table_name='words_enhanced'
-  `
-
-  connection.query(readyToRunCheck, (err, result) => {
-    if(err) throw err
-
-    if(result.length == 0) {
-      console.log(`\nFIRST RUN THE SCRIPT TO BUILD OUT WORDS_ENHANCED: node run.js\n`)
-      process.exit()
-      return
-    }
-
-    utils.deleteRowsMadeByScript({ connection, enhancedTables: false }, () => {
-
-      // console.log(`\nCreating 'bad' column in notes table...`)
+  utils.runInSeries([
     
-      // const addColStatement = `ALTER TABLE notes ADD COLUMN bad TINYINT(1) DEFAULT 0 AFTER verification`
-    
-      // connection.query(addColStatement, (err, result) => {
-      //   // it is okay if it throws an error, as this mean the column already exists
-      //   // if(err) throw err
-      //   console.log(`    - done.`)
-
-      //   console.log(`\nMarking notes rows 'bad' where appropriate...`)
-        
-      //   // mark bad:1 where a row's word in words_enhanced is status:none
-      //   const selectBadNoteIds = `
-      //     SELECT notes.id
-      //     FROM notes
-      //       LEFT JOIN wordnote ON (wordnote.noteId = notes.id)
-      //       LEFT JOIN words_enhanced ON (wordnote.wordId = words_enhanced.id)
-      //     WHERE 
-      //       words_enhanced.status NOT IN ('none', 'single', 'confirmed', 'verified')
-      //   `
-
-      //   connection.query(selectBadNoteIds, (err, result) => {
-      //     if(err) throw err
-
-      //     const updates = [
-      //       `UPDATE notes SET bad=0`,
-      //       ...result.map(row => `
-      //         UPDATE notes SET bad=1 WHERE id=${row.id}
-      //       `)
-      //     ]
+    (x, next) => {
       
-      //     utils.doUpdatesInChunks(connection, { updates }, numRowsUpdated => {
-      //       console.log(`    - ${result.length} notes rows marked bad.`)
+      console.log(`  Widen word column in words table...`)
+      
+      const statement = `
+        ALTER TABLE words MODIFY COLUMN word VARCHAR(25);
+      `
 
-            // add in notes and word_note table rows for every word in words_enhanced where the morph
-            // code is not represented for that word in a notes table row (give these inserted rows memberId=416)
+      connection.query(statement, (err, result) => {
+        if(err) throw err
 
-            const selectAllWords = `
-              SELECT words_enhanced.id, words_enhanced.morph, notes.morph as notesMorph
-              FROM words_enhanced
-                LEFT JOIN wordnote ON (wordnote.wordId = words_enhanced.id)
-                LEFT JOIN notes ON (wordnote.noteId = notes.id)
-              WHERE words_enhanced.morph IS NOT NULL
-              ORDER BY words_enhanced.id
-            `
+        console.log(`    - done.`)
+        next()
+      })
+    
+    },
 
-            connection.query(selectAllWords, (err, result) => {
-              if(err) throw err
+    (x, next) => {
+      
+      console.log(`  Fix words where they were cut-off or incorrect...`)
 
-              console.log(`\nCreating new notes rows where appropriate...`)
+      const statement = `
+        UPDATE word SET word="אֶחָֽד", append="׃" WHERE bookId=5 AND chapter=6 AND verse=4 AND number=5;
+      `
 
-              let words = []
+      connection.query(statement, (err, result) => {
+        if(err) throw err
 
-              result.forEach(word => {
-                if(words.length > 0 && word.id == words[words.length - 1].id) {
-                  if(word.notesMorph) {
-                    words[words.length - 1].notesMorph.push(word.notesMorph)
-                  }
-                } else {
-                  word.notesMorph = [ word.notesMorph ]
-                  words.push(word)
-                }
-              })
+        console.log(`    - done.`)
+        next()
+      })
+    
+    },
 
-              words = words.filter(word => !word.notesMorph.includes(word.morph))
+    (x, next) => {
+      
+      console.log(`  Divide words where they should have been divided...`)
 
-              const updates = words.map(word => `
-                INSERT INTO notes
-                  (memberId, noteDate, morph, verification)
-                  VALUES (416, '0000-01-01 00:00:00', '${word.morph}', 0)
-              `)
-
-              const addlUpdates = []
-              const resultCallback = (result, index) => {
-                addlUpdates.push(`INSERT INTO wordnote
-                  (wordId, noteId)
-                  VALUES (${words[index].id}, ${result.insertId})
-                `)
-              }
-
-              utils.doUpdatesInChunks(connection, { updates, resultCallback }, numRowsUpdated => {
-                console.log(`    - ${numRowsUpdated} notes rows created.`)
-
-                console.log(`\nCreating new wordnote rows where appropriate...`)
-
-                utils.doUpdatesInChunks(connection, { updates: addlUpdates }, numRowsUpdated => {
-                  console.log(`    - ${numRowsUpdated} wordnote rows created.`)
-
-                  console.log(`\nDrop indexes created for words_enhanced...`)
-
-                  utils.dropIndexes(connection, {
-                    indexes: [
-                      {
-                        table: 'words',
-                        col: 'morph',
-                      },
-                      {
-                        table: 'words',
-                        col: 'word',
-                      },
-                      {
-                        table: 'words',
-                        col: 'lemma',
-                      },
-                      {
-                        table: 'words',
-                        col: 'status',
-                      },
-                    ],
-                  }, () => {
+      const updates = []
+      ;[
+        [1,14,17,8,"כְּדָר","לָעֹ֔מֶר","3540"],
+      ].forEach(updateInfo => {
+        updates.push(`UPDATE words SET number=number+1 WHERE bookId=${updateInfo[0]} AND chapter=${updateInfo[1]} AND verse=${updateInfo[2]} AND number>${updateInfo[3]}`)
+        updates.push(`UPDATE words SET word="${updateInfo[4]}" WHERE bookId=${updateInfo[0]} AND chapter=${updateInfo[1]} AND verse=${updateInfo[2]} AND number=${updateInfo[3]}`)
+        updates.push(`
+          INSERT INTO words
+            (bookId, chapter, verse, number, word, lemma, wordtype, status)
+            VALUES ("${updateInfo[0]}", "${updateInfo[1]}", "${updateInfo[2]}", "${updateInfo[3]+1}", "${updateInfo[5]}", "${updateInfo[6]}", "word", "none")
+        `)
+      })
+      
+      utils.doUpdatesInChunks(connection, { updates }, numRowsUpdated => {
+        console.log(`    - done.`)
+        next()                
+      })
               
-                    console.log(`\nSwap words_enhanced in for words...`)
-                    // drop accentlessword column in words_enhanced table
-                    // rename words to words_[date]
-                    // rename words_enhanced to words
-                    const statementSet = `
-                      ALTER TABLE words_enhanced DROP COLUMN accentlessword;
-                      RENAME TABLE words TO words_${new Date().toString().replace(/[ :+\(\)]/g, '_')};
-                      RENAME TABLE words_enhanced TO words;
-                    `
+    },
 
-                    connection.query(statementSet, (err, result) => {
-                      if(err) throw err
-                              
-                      console.log(`\nCOMPLETED\n`)
-                      process.exit()
-            
-                    })            
-                  })            
-                })            
-              })            
+    (x, next) => {
+      
+      console.log(`  Reindexing...`)
 
-            })            
-      //     })
-      //   })
-      // })
-    })
-  })
-});
+      const statement = `
+        DROP TABLE IF EXISTS words_temp; 
+        CREATE TABLE words_temp LIKE words;
+        INSERT words_temp
+          SELECT
+            NULL as id,
+            bookId,
+            chapter,
+            verse,
+            number,
+            word,
+            append,
+            lemma,
+            morph,
+            wordtype,
+            status
+          FROM words
+          ORDER BY bookId, chapter, verse, number;
+        DROP TABLE IF EXISTS words; 
+        RENAME TABLE words_temp TO words;
+      `
+
+      connection.query(statement, (err, result) => {
+        if(err) throw err
+
+        console.log(`    - done.`)
+        next()
+      })
+
+    },
+
+    () => {
+      console.log(`\n !!! Need to have this update the wordnote table as well!!`)
+      console.log(`\nCOMPLETED\n`)
+      process.exit()
+    }
+    
+  ])
+})
