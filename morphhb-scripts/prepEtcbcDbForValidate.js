f0b7912a62a613c00d1371ce23368af25d1bd9e4
Huberts <huberts@Hubertss-MacBook-Pro.local>
2017-10-15 00:04:38 +0300

    create etcbc table and remove qere

--- morphhb-scripts/prepEtcbcDbForValidate.js
+++ morphhb-scripts/prepEtcbcDbForValidate.js
@@ -21,56 +21,105 @@ connection.connect(function(err) {
     
     (x, next) => {
       
-      console.log(`  Deleting _enhanced tables since this will invalidate them...`)
-
-      const statement = `
-        DROP TABLE IF EXISTS words_enhanced; 
-        DROP TABLE IF EXISTS wordnote_enhanced; 
-        DROP TABLE IF EXISTS notes_enhanced; 
-      `
-
-      connection.query(statement, (err, result) => {
-        if(err) throw err
-
-        console.log(`    - done.`)
-        next()
-      })
-    
-    },
-
-    // make like words_enhanced schema
-    // get rid of the 5 etcbc qere spots
-    // split on space or dash, noting that there are more than one/verse sometimes
-    // add blank row where there is qere
-
-    (x, next) => {
-      
-      console.log(`  Split words where they should be split but are not, and re-index everything...`)
-
-      const statement = `
-      `
-
-      connection.query(statement, (err, result) => {
-        if(err) throw err
-
-        console.log(`    - done.`)
-        next()
-      })
-    
-    },
-    (x, next) => {
-      
       console.log(`  Make a copy of the table with a schema that accords with words...`)
       
+      const statement = `
+        DROP TABLE IF EXISTS etcbc; 
+        CREATE TABLE etcbc LIKE words;
+        ALTER TABLE etcbc MODIFY COLUMN word VARCHAR(50);
+        INSERT etcbc
+          SELECT
+            NULL as id,
+            bk_num as bookId,
+            ch_num as chapter,
+            vs_num as verse,
+            vs_word_num as number,
+            heb_word as word,
+            NULL as append,
+            "" as lemma,
+            morph_code as morph,
+            "word" as wordtype,
+            "none" as status
+          FROM etcbc_words_combined;
+      `
+
+      connection.query(statement, (err, result) => {
+        if(err) throw err
+
+        console.log(`    - done.`)
+        next()
+      })
+    
     },
 
     (x, next) => {
       
-      console.log(`  Correct number column values where off...`)
+      console.log(`  Removing 6 etcbc qere word additions...`)
+
+      const statement = `
+        UPDATE etcbc SET word="את" WHERE bookId=13 AND chapter=38 AND verse=16 AND number=9;
+        UPDATE etcbc SET word="אם" WHERE bookId=13 AND chapter=39 AND verse=12 AND number=9;
+        UPDATE etcbc SET word="ידרך" WHERE bookId=13 AND chapter=51 AND verse=3 AND number=1;
+        UPDATE etcbc SET word="חמשׁ" WHERE bookId=14 AND chapter=48 AND verse=16 AND number=10;
+        UPDATE etcbc SET word="אם" WHERE bookId=31 AND chapter=3 AND verse=12 AND number=3;
+        UPDATE etcbc SET word="בתיהם" WHERE bookId=39 AND chapter=34 AND verse=6 AND number=6;
+      `
+
+      connection.query(statement, (err, result) => {
+        if(err) throw err
+
+        console.log(`    - done.`)
+        next()
+      })
+    
+    },
+
+    // split on space or dash, noting that there are more than one/verse sometimes
+    // 
+
+    (x, next) => {
+      
+      console.log(`  Splitting on space or dash...`)
+
+      // const statement = `
+      // `
+
+      // connection.query(statement, (err, result) => {
+      //   if(err) throw err
+
+      //   console.log(`    - done.`)
+      //   next()
+      // })
+      next()
+    
+    },
+
+    (x, next) => {
+      
+      console.log(`  Adding blank rows where there is qere...`)
 
       // ETCBC has no qere
       // 2000 places where the words do not match up
       // ------Between OSHB and ETCBC, sometimes words (especially place names) are stored together in one row or separately in different rows. Eg, Gen 4:22; 14:7.
+      next()
+
+    },
+
+    (x, next) => {
+      
+      console.log(`  Reindexing...`)
+
+      // ETCBC has no qere
+      // 2000 places where the words do not match up
+      // ------Between OSHB and ETCBC, sometimes words (especially place names) are stored together in one row or separately in different rows. Eg, Gen 4:22; 14:7.
+      next()
+
+    },
+
+    (x, next) => {
+      
+      console.log(`  Check for alignment...`)
+      next()
       
     },
 
@@ -79,7 +128,8 @@ connection.connect(function(err) {
       console.log(`  Correct morph of prepositions with definite article...`)
 
       // May need to modify code so that Prepositions followed by Definite Article only have Rd morph code, instead of RTd, since ETCBC stores definite article in its own row after the preposition row.
-      
+      next()
+
     },
 
     (x, next) => {
@@ -88,7 +138,8 @@ connection.connect(function(err) {
 
       // ETCBC does not parse suffixes, although they are (to some degree) stored in a separate column in the database. OSHB does parse them, so need to work with that.
       // Often when a word has a suffix, ETCBC parses it as absolute, whereas OSHB parses it as construct.
-      
+      next()
+
     },
 
     (x, next) => {
@@ -99,7 +150,8 @@ connection.connect(function(err) {
       // 1cp
       // 3cp
       // In ETCBC, where 3rd common is an option in OSHB, the ETCBC will sometimes NOT use any gender. Eg. HC/Vqp3p instead of HC/Vqq3cp
-      
+      next()
+
     },
 
     () => {
