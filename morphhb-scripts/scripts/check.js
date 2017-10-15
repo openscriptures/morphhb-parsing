5522092c032baba328bcd486f2489661b91b3427
Huberts <huberts@Hubertss-MacBook-Pro.local>
2017-10-15 17:31:09 +0300

    Comment change

--- morphhb-scripts/scripts/check.js
+++ morphhb-scripts/scripts/check.js
@@ -6,7 +6,6 @@ module.exports = (connection, done) => {
 
   // ETCBC doesn't use WeQatal verb stem, whereas OSHB may.
   // Adjective in ETCBC do not have cardinal or ordinal types
-
   // ETCBC does not parse suffixes, although they are (to some degree) stored in a separate column in the database. OSHB does parse them, so need to work with that.
   // Often when a word has a suffix, ETCBC parses it as absolute, whereas OSHB parses it as construct.
   
