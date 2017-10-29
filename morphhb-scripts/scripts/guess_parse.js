0330b8ee4c40759faa384ab575c66e5526c0f080
Andy Hubert <andy.hubert+8@gmail.com>
2017-10-29 10:34:10 +0200

    guess parse state of nouns, etc that can be either construct or absolute

--- morphhb-scripts/scripts/guess_parse.js
+++ morphhb-scripts/scripts/guess_parse.js
@@ -18,6 +18,8 @@ module.exports = (connection, done) => {
     result = result.filter(row => !blacklistedForms.includes(row.accentlessword))
     
     let uniqueWordsCount = 0
+    let totalWordsWithUnknownState = 0
+    let totalNotSimpleToParse = 0
     const updateWordQueries = []
     const outlierOutput = []
 
@@ -64,56 +66,174 @@ module.exports = (connection, done) => {
           // Look for situations where simple auto-parsing is not possible
           // - conj + perfect vs sequential perfect
           // - 2nd/3rd jussive vs 2nd/3rd imperfect
-          // - absolute vs construct
           // - both vs masc vs fem
           // - imperfect 3fs vs 3ms  "HVqi3fs", (81%) -- but HVqi2ms
+          // - absolute vs construct
           const noSimpleAutoParse = mainMorph && outliers.length > 0 && outliers.some(outlier => {
             return [
               [/^(HC\/V[^\/])[pq]([^\/][^\/][^\/])/, '$1-$2'],
               [/^(H(?:[^\/]*\/)*V[^\/])[ij]([23][^\/][^\/])/, '$1-$2'],
-              [/^(H(?:[^\/]*\/)*(?:N[^\/][^\/][^\/]|A[^\/][^\/][^\/]|V[^\/][rs][^\/][^\/]))[ac]/, '$1-'],
-              [/^(H(?:[^\ /]*\/)*(?:N[^\/]|A[^\/]))[bfm]/, '$1-'],
+              [/^(H(?:[^\ /]*\/)*(?:N[^\/]|A[^\/]))[fm]/, '$1b'],
               [/^(H(?:[^\ /]*\/)*V[^\/][iw])(?:3fs|2ms)/, '$1-'],
+            ].some(replaceCheck => (
+              mainMorph.replace(replaceCheck[0], replaceCheck[1]) == outlier.morph.replace(replaceCheck[0], replaceCheck[1])
+            ))
+          })
+          const canBeAbsoluteOrConstruct = mainMorph && outliers.length > 0 && outliers.some(outlier => {
+            return [
+              [/^(H(?:[^\/]*\/)*(?:N[^\/][^\/][^\/]|A[^\/][^\/][^\/]|V[^\/][rs][^\/][^\/]))[ac]/, '$1-'],
             ].some(replaceCheck => (
               mainMorph.replace(replaceCheck[0], replaceCheck[1]) == outlier.morph.replace(replaceCheck[0], replaceCheck[1])
             ))
           })
                           
           if(mainMorph && totalWithThisForm >= 2 && !noSimpleAutoParse) {
-            uniqueWordsCount++
-            updateWordQueries.push(`UPDATE words_enhanced SET morph='${mainMorph}', status='single' ${autoParseWhere}`)
 
-            if(outliers.length > 0) {
-              const selectWordWithLocInfo = outliers.map(outlier => `
-                SELECT bookId, chapter, verse FROM words_enhanced
-                  WHERE
-                    accentlessword="${rowWithoutMorph.accentlessword}"
-                    AND lemma="${rowWithoutMorph.lemma}"
-                    AND morph="${outlier.morph}"
-                    AND status IN ('single', 'confirmed', 'verified')
-                  LIMIT 1
-              `)
-              connection.query(selectWordWithLocInfo.join(';'), (err, results) => {
+            if(canBeAbsoluteOrConstruct) {
+              // each instance needs to be evaluated to be construct or absolute based on context
+
+              const stateSelect1 = `SELECT * FROM words_enhanced ${autoParseWhere}`
+              connection.query(stateSelect1, (err, stateResults1) => {
                 if(err) throw err
 
-                if(selectWordWithLocInfo.length == 1) {
-                  results = [ results ]
-                }
-              
-                const outlierExamples = []
-                let totalOutliers = 0
-                results.forEach((result, index) => {
-                  totalOutliers += outliers[index].cnt
-                  outlierExamples.push(`${outliers[index].morph} in ${utils.getBibleBookName(result[0].bookId)} ${result[0].chapter}:${result[0].verse} `
-                    + (outliers[index].cnt > 1 ? `(+${outliers[index].cnt-1} more)` : ``))
-                })
-                outlierOutput.push(`${('000' + totalOutliers).substr(-3)}  >> "${rowWithoutMorph.accentlessword}": "${mainMorph}", (${mainMorphPercentage}%) -- but ${outlierExamples.join(', ')}`)
+                let extraQueries = 0
+                let doneWithMainLoop = false
 
-                tryNextWord()
+                const constructMorph = mainMorph.replace(/^(H(?:[^\/]*\/)*(?:N[^\/][^\/][^\/]|A[^\/][^\/][^\/]|V[^\/][rs][^\/][^\/]))[ac]/, '$1c')
+                const absoluteMorph = mainMorph.replace(/^(H(?:[^\/]*\/)*(?:N[^\/][^\/][^\/]|A[^\/][^\/][^\/]|V[^\/][rs][^\/][^\/]))[ac]/, '$1a')
+
+                stateResults1.forEach(stateRow1 => {
+                  if(
+                    stateRow1.append == "־"
+                    || stateRow1.accentlessword.match(/ֵי$/)
+                  ) {
+                    // construct if
+                      // with dash
+                      // ends with 2dots-yud
+                    uniqueWordsCount++
+                    updateWordQueries.push(`UPDATE words_enhanced SET morph='${constructMorph}', status='single' WHERE id=${stateRow1.id}`)
+                  } else if(
+                    stateRow1.accentlessword.match(/ִים$/)
+                    || stateRow1.accentlessword.match(/ָה$/)
+                    || stateRow1.accentlessword.match(/^הַ\//)
+                  ) {
+                    // absolute if
+                      // ends in yud-mem OR patach-hey
+                      // has definite article
+                    uniqueWordsCount++
+                    updateWordQueries.push(`UPDATE words_enhanced SET morph='${absoluteMorph}', status='single' WHERE id=${stateRow1.id}`)
+                  } else {
+
+                    extraQueries++
+
+                    const stateSelect2 = `
+                      SELECT * FROM words_enhanced
+                        WHERE
+                          bookId=${stateRow1.bookId}
+                          AND chapter=${stateRow1.chapter}
+                          AND verse=${stateRow1.verse}
+                        ORDER BY number
+                    `
+                    connection.query(stateSelect2, (err, stateResults2) => {
+                      if(err) throw err
+
+                      const prevWord = stateRow1.wordtype == 'qere' ? stateResults2[stateRow1.number-2] : stateResults2[stateRow1.number-1]
+                      const nextWord = !stateResults2[stateRow1.number+1]
+                        ? null
+                        : stateResults2[stateRow1.number+1].wordtype == 'qere'
+                          ? stateResults2[stateRow1.number+2]
+                          : stateResults2[stateRow1.number+1]
+
+                      if(
+                        stateRow1.number == stateResults2.length - 1
+                        || (
+                          prevWord
+                          && prevWord.append == "־"
+                          && stateRow1.append != "־"
+                        )
+                        || (nextWord && nextWord.morph && !nextWord.morph.match(/^H(?:[^\/]*\/)*(?:N|A|V[^\/][rs])/))
+                        || (nextWord && nextWord.morph && nextWord.morph.match(/^H[CR]/))
+                      ) {
+                        // absolute if
+                          // last in verse
+                          // dash before but not after
+                          // following word is
+                            // not noun, adj or participle
+                            // has a preposition or conjunction
+                        uniqueWordsCount++
+                        updateWordQueries.push(`UPDATE words_enhanced SET morph='${absoluteMorph}', status='single' WHERE id=${stateRow1.id}`)
+                      } else if(
+                        (nextWord && nextWord.morph && nextWord.morph.match(/^H(?:[^\/]*\/)*Np/))
+                        || (nextWord && nextWord.morph && nextWord.morph.match(/^HTd/))
+                      ) {
+                        // construct if
+                          // proper name follows
+                          // noun with definite article follows
+                        uniqueWordsCount++
+                        updateWordQueries.push(`UPDATE words_enhanced SET morph='${constructMorph}', status='single' WHERE id=${stateRow1.id}`)
+                      } else {
+                        totalWordsWithUnknownState++
+                      }
+
+                      if(--extraQueries == 0 && doneWithMainLoop) tryNextWord()
+                          
+                    })
+
+                  }
+                })
+
+                doneWithMainLoop = true
+                if(extraQueries == 0) tryNextWord()
+
               })
+                    
             } else {
-              tryNextWord()
+              uniqueWordsCount++
+              updateWordQueries.push(`UPDATE words_enhanced SET morph='${mainMorph}', status='single' ${autoParseWhere}`)
+
+              if(outliers.length > 0) {
+                const selectWordWithLocInfo = outliers.map(outlier => `
+                  SELECT bookId, chapter, verse FROM words_enhanced
+                    WHERE
+                      accentlessword="${rowWithoutMorph.accentlessword}"
+                      AND lemma="${rowWithoutMorph.lemma}"
+                      AND morph="${outlier.morph}"
+                      AND status IN ('single', 'confirmed', 'verified')
+                    LIMIT 1
+                `)
+                connection.query(selectWordWithLocInfo.join(';'), (err, results) => {
+                  if(err) throw err
+  
+                  if(selectWordWithLocInfo.length == 1) {
+                    results = [ results ]
+                  }
+                
+                  const outlierExamples = []
+                  let totalOutliers = 0
+                  results.forEach((result, index) => {
+                    totalOutliers += outliers[index].cnt
+                    outlierExamples.push(`${outliers[index].morph} in ${utils.getBibleBookName(result[0].bookId)} ${result[0].chapter}:${result[0].verse} `
+                      + (outliers[index].cnt > 1 ? `(+${outliers[index].cnt-1} more)` : ``))
+                  })
+                  outlierOutput.push(`${('000' + totalOutliers).substr(-3)}  >> "${rowWithoutMorph.accentlessword}": "${mainMorph}", (${mainMorphPercentage}%) -- but ${outlierExamples.join(', ')}`)
+  
+                  tryNextWord()
+                })
+              } else {
+                tryNextWord()
+              }
             }
+
+          } else if(mainMorph) {
+
+            const select = `SELECT * FROM words_enhanced ${autoParseWhere}`
+            connection.query(select, (err, results) => {
+              if(err) throw err
+
+              totalNotSimpleToParse += results.length
+              tryNextWord()
+            })
+
           } else {
             tryNextWord()
           }
@@ -126,6 +246,8 @@ module.exports = (connection, done) => {
         outlierOutput.forEach(outlierLine => {
           console.log(outlierLine.substr(3))
         })
+        console.log(`  ** ${totalWordsWithUnknownState} words can be guess-parsed if I can determine their state`)
+        console.log(`  ** ${totalNotSimpleToParse} have multiple valid parsings and thus were not guess-parsed`)
         
         // run updates
         utils.doUpdatesInChunks(connection, { updates: updateWordQueries }, numRowsUpdated => {
