043029df4f6a2ebf5cc26dd73601ed15e9da5e3b
Andy Hubert <andy.hubert+8@gmail.com>
2017-11-21 17:28:58 +0200

    Got xml import / export working

---
+++ morphhb-scripts/exportToOsis.js
@@ -0,0 +1,176 @@
+require('dotenv').config()
+
+const mysql = require('mysql')
+const xml2js = require('xml2js')
+const js2xmlparser = require("js2xmlparser")
+const fs = require('fs')
+const shell = require('shelljs')
+
+const utils = require('./utils')
+
+const connection = mysql.createConnection({
+  host: process.env.DB_NAME || "localhost",
+  database: process.env.HOSTNAME || 'oshb',
+  user: process.env.USERNAME || "root",
+  password: process.env.PASSWORD || "",
+  multipleStatements: true,
+})
+
+connection.connect(function(err) {
+  if(err) throw err
+
+  console.log(`\nSTARTING`)
+
+  const bookURIsByBookId = {}
+  const importDir = '../../morphhb/wlc'
+  const exportDir = "./morphhb"
+
+  utils.runInSeries([
+    
+    // (x, next) => {
+
+    //   var parser = new xml2js.Parser({
+    //     explicitChildren: true,
+    //     preserveChildrenOrder: true,
+    //     attrkey: '@',
+    //   })
+
+    //   const dirname = '../../morphhb/wlc'
+    //   const filename = `Obad.xml`
+
+    //   fs.readFile(`${dirname}/${filename}`, 'utf-8', (err, xml) => {
+    //     console.log(`    ${filename}...`)
+
+    //     if(err) { 
+    //       console.log(err)
+    //       process.exit()
+    //     }              
+              
+    //     parser.parseString(xml, function (err, result) {
+
+    //       console.log(JSON.stringify(result))
+
+    //       process.exit()
+          
+    //     })
+    //   })
+
+    // },
+
+    (x, next) => {
+
+      fs.readdir(importDir, (err, filenames) => {
+        if(err) { 
+          console.log(err)
+          process.exit()
+        }              
+
+        filenames = filenames.filter(filename => filename.match(/^\w/) && !['VerseMap.xml'].includes(filename))
+
+        filenames.forEach(filename => {
+          const bookId = utils.getBibleBookIdByAbbr(filename.replace(/\..*$/, ''))
+          bookURIsByBookId[bookId] = filename
+        })
+
+        next()
+        
+      })
+
+    },
+
+    (x, next) => {
+
+      const parser = new xml2js.Parser({
+        explicitChildren: true,
+        preserveChildrenOrder: true,
+      })
+
+      let bookId = 1
+
+      const exportBook = () => {
+
+        if(bookId > 39) {
+
+          next()
+          return
+        }
+
+        const statement = `SELECT * FROM words WHERE bookId=${bookId} ORDER BY id`
+
+        connection.query(statement, (err, result) => {
+          if(err) throw err
+
+          fs.readFile(`${importDir}/${bookURIsByBookId[bookId]}`, 'utf-8', (err, xml) => {
+            console.log(`  Began ${bookURIsByBookId[bookId]}...`)
+
+            if(err) { 
+              console.log(err)
+              process.exit()
+            }
+
+            parser.parseString(xml, function (err, result) {
+
+              const convertObj = obj => {
+                const newObj = {
+                  "=": obj["#name"]
+                }
+                if(obj["$"]) {
+                  newObj["@"] = obj["$"]
+                }
+                if(obj["$$"]) {
+                  newObj["group"] = obj["$$"].map(child => convertObj(child))
+                } else if(obj["_"]) {
+                  newObj["#"] = obj["_"]
+                }
+
+                return newObj
+              }
+                      
+              result.osis = convertObj(result.osis)
+
+              let newXml = js2xmlparser.parse('osis', result.osis, {
+                declaration: {
+                  encoding: "utf-8",
+                },
+                format: {
+                  doubleQuotes: true,
+                  indent: "  ",
+                },
+              })
+
+              // format newlines like the morphhb files
+              newXml = newXml
+                .replace(/(>)\n *(<seg type="x-sof-pasuq">׃<\/seg>)/g, "$1$2")
+                .replace(/(<\/w>)\n *(<seg type="x-maqqef">־<\/seg>)\n *(<w )/g, "$1$2$3")
+                .replace(/(<\/w>)\n *(<note type="[^"]*">)\n *(<catchWord>[^<]*<\/catchWord>)\n *(<rdg type="[^"]*">[^<]*<\/rdg>)\n *(<\/note>)/g, "$1$2$3$4$5")
+                .replace(/(<\/w>)\n *(<note type="[^"]*">)\n *(<catchWord>[^<]*<\/catchWord>)\n *(<rdg type="[^"]*">)\n *([^\n]*)\n *(<\/rdg>)\n *(<\/note>)/g, "$1$2$3$4$5$6$7")
+
+              fs.writeFile(`${exportDir}/${bookURIsByBookId[bookId]}`, newXml, function(err) {
+                  if(err) {
+                      return console.log(err);
+                  }
+        
+                  console.log(`    Wrote ${bookURIsByBookId[bookId]}.`)
+
+                  shell.exec(`diff ${importDir}/${bookURIsByBookId[bookId]} ${exportDir}/${bookURIsByBookId[bookId]}`)
+
+                  bookId++ && exportBook()
+              })
+
+            })
+
+          })
+        })
+      }
+            
+      exportBook()
+                  
+    },
+
+    () => {
+      console.log(`\nCOMPLETED\n`)
+      process.exit()
+    }
+    
+  ])
+})
